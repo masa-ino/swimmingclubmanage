c765282db9ff5128c060248c7362f58f1dd65520
HORIZONium <masayuki121110@icloud.com>
2020-06-10 14:25:13 +0900

    zzz

--- dbconnect.php
+++ dbconnect.php
@@ -1,18 +1,21 @@
 <?php
+// try{
+//   $db = new PDO('mysql:dbname=swimming; host=127.0.0.1; charset =utf8','root','Masa0077');
+// }catch(PDOException $e){
+//   print('DB接続エラー:'. $e->getMessage());
+// }
+?> 
 
-function dbConnect(){
-  $db = parse_url($_SERVER['CLEARDB_DATABASE_URL']);
-  $db['dbname'] = ltrim($db['path'], '/');
-  $dsn = "mysql:host={$db['host']};dbname={$db['dbname']};charset=utf8";
-  $user = $db['user'];
-  $password = $db['pass'];
-  $options = array(
-    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-    PDO::MYSQL_ATTR_USE_BUFFERED_QUERY =>true,
-  );
-  $dbh = new PDO($dsn,$user,$password,$options);
-  return $dbh;
-}
-
+<?php
+$db = parse_url($_SERVER['CLEARDB_DATABASE_URL']);
+$db['dbname'] = ltrim($db['path'], '/');
+$dsn = "mysql:host={$db['host']};dbname={$db['dbname']};charset=utf8";
+$user = $db['user'];
+$password = $db['pass'];
+$options = array(
+  PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+  PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+  PDO::MYSQL_ATTR_USE_BUFFERED_QUERY =>true,
+);
+$dbh = new PDO($dsn,$user,$password,$options);
 ?>
