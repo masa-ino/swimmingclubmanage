a154cd3dd90f89a1bb1514608ce402cc6db0eabb
HORIZONium <masayuki121110@icloud.com>
2020-06-11 08:45:41 +0900

    q

---
+++ member.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+
+<html lang="ja">
+
+<head>
+  <meta charset="utf-8">
+  <meta name="author" content="">
+  <title></title>
+  <meta name="description" content="">
+  <meta name="keywords" content="">
+  <meta name="viewport" content="width=device-width,initial-scale=1">
+
+  <link rel="icon" href="img/favicon.ico">
+
+  <!-- Style CSS -->
+  <link href="css/style.css" rel="stylesheet">
+  <link href="css/uikit.min.css" rel="stylesheet">
+
+  <!-- Font Awesome Icons -->
+  <link href="https://use.fontawesome.com/releases/v5.6.1/css/all.css" rel="stylesheet">
+
+  <link href="https://fonts.googleapis.com/css2?family=Noto+Sans&family=Noto+Sans+JP:wght@100;400;900&display=swap"
+    rel="stylesheet">
+
+</head>
+
+<body>
+  <header>
+    <nav class="uk-navbar-container uk-margin" uk-navbar>
+      <div class="uk-navbar-left">
+        <a class="uk-navbar-item uk-logo" href="index.php">水泳同好会前払いページ</a>
+      </div>
+    </nav>
+  </header>
+  <main class="uk-container uk-container-small">
+        <h1 class="uk-heading-line uk-text-center"><span>メンバー編集</span></h1>
+      <div class="uk-text-center">
+        <a href ="add_member.php" class="uk-button uk-button-primary">追加</a>
+        <a href ="delete_member.php" class="uk-button uk-button-primary">削除</a>
+      </div>
+    </form>
+
+
+  </main>
+  <footer>
+
+  </footer>
+  <script src="js/jquery-3.5.1.min.js"></script>
+  <script src="js/uikit.min.js"></script>
+  </footer>
+</body>
+
+</html>
