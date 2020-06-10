baeb74822affa3ce1bb7ed2c652dea29294b040a
HORIZONium <masayuki121110@icloud.com>
2020-06-10 11:16:08 +0900

    aa

--- dbconnect.php
+++ dbconnect.php
@@ -1,24 +1,18 @@
 <?php
 
-$host = getenv('host'); //MySQLがインストールされてるコンピュータ
-$dbname = getenv('db'); //使用するDB
-$charset = "utf8"; //文字コード
-$user = getenv('user'); //MySQLにログインするユーザー名
-$password = getenv('pass'); //ユーザーのパスワード
-
-$options = [
-    PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION, //SQLでエラーが表示された場合、画面にエラーが出力される
-    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC, //DBから取得したデータを連想配列の形式で取得する
-    PDO::ATTR_EMULATE_PREPARES   => false, //SQLインジェクション対策
-];
-
-//DBへの接続設定
-$dsn = "mysql:host=$host;dbname=$dbname;charset=$charset";
-try {
-    //DBへ接続
-    $dbh = new PDO($dsn, $user, $password, $options);
-} catch (\PDOException $e) {
-    throw new \PDOException($e->getMessage(), (int) $e->getCode());
+function dbConnect(){
+  $db = parse_url($_SERVER['CLEARDB_DATABASE_URL']);
+  $db['dbname'] = ltrim($db['path'], '/');
+  $dsn = "mysql:host={$db['host']};dbname={$db['dbname']};charset=utf8";
+  $user = $db['user'];
+  $password = $db['pass'];
+  $options = array(
+    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+    PDO::MYSQL_ATTR_USE_BUFFERED_QUERY =>true,
+  );
+  $dbh = new PDO($dsn,$user,$password,$options);
+  return $dbh;
 }
 
 ?>
