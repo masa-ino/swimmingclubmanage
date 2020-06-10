d494cbf05c7fd8960194bb26c92fa26340d385a3
HORIZONium <masayuki121110@icloud.com>
2020-06-10 10:47:58 +0900

    create sql

--- dbconnect.php
+++ dbconnect.php
@@ -1,7 +1,22 @@
 <?php
-try{
-  $db = new PDO('mysql:dbname=swimming; host=127.0.0.1; charset =utf8','root','Masa0077');
-}catch(PDOException $e){
-  print('DB接続エラー:'. $e->getMessage());
+
+$host = getenv('host'); //MySQLがインストールされてるコンピュータ
+$dbname = getenv('db'); //使用するDB
+$charset = "utf8"; //文字コード
+$user = getenv('user'); //MySQLにログインするユーザー名
+$password = getenv('pass'); //ユーザーのパスワード
+
+$options = [
+    PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION, //SQLでエラーが表示された場合、画面にエラーが出力される
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC, //DBから取得したデータを連想配列の形式で取得する
+    PDO::ATTR_EMULATE_PREPARES   => false, //SQLインジェクション対策
+];
+
+//DBへの接続設定
+$dsn = "mysql:host=$host;dbname=$dbname;charset=$charset";
+try {
+    //DBへ接続
+    $dbh = new PDO($dsn, $user, $password, $options);
+} catch (\PDOException $e) {
+    throw new \PDOException($e->getMessage(), (int) $e->getCode());
 }
-?>
